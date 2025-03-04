3fec2f4fef562ac96d6de0f43e87dfa7f2fc00cb
peace1313 <122394469+peace1313@users.noreply.github.com>
2025-03-04 23:24:51 +0300

    Update PaymentProcess.php

--- src/PaymentProcess.php
+++ src/PaymentProcess.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Exception\RequestException;
 use Peace1313\GPaymentSdk\Config\Config;
 use Peace1313\GPaymentSdk\Exception\PaymentException;
 
-class PaymentProcessor
+class PaymentProcess
 {
     private Client $client;
     private string $apiUrl;
