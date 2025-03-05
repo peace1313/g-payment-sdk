ee5f955f771f4399792849b9d9576a284434cd2a
peace1313 <122394469+peace1313@users.noreply.github.com>
2025-03-05 22:32:29 +0300

    Add files via upload

--- src/PaymentProcessor.php
+++ src/PaymentProcessor.php
@@ -6,16 +6,20 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Peace1313\GPaymentSdk\Config\Config;
 use Peace1313\GPaymentSdk\Exception\PaymentException;
-
+use Peace1313\GPaymentSdk\Helper\HelperFunction;
 class PaymentProcessor
 {
     private Client $client;
     private string $apiUrl;
     private string $swtId;
     private string $swtPassword;
+    private string $serviceUser;
+    private string $servicePass;
 
     public function __construct()
     {
+        $this->helper=new HelperFunction();
+
         $this->client = new Client([
             'base_uri' => Config::API_URL,
             'timeout'  => 10.0,
@@ -25,33 +29,49 @@ class PaymentProcessor
         $this->apiUrl = Config::API_URL;
         $this->swtId = Config::SWT_ID;
         $this->swtPassword = Config::SWT_PASSWORD;
+        $this->serviceUser  =Config::SERVICE_USER;
+        $this->servicePass  =Config::SERVICE_PASS;
+        $this->userId  =Config::USER_ID;
     }
 
-    public function makeRequest(string $endpoint, array $body, string $userId)
+    public function makeRequest(string $endpoint, array $body ,array $params=array() )
     {
-        $requestId = bin2hex(random_bytes(16));
+        $requestId = $this->helper->generateUUIDv4();
         $timestamp = time();
-        $hashedData = strtoupper(hash('sha256', $requestId . $this->swtId . $userId . $timestamp . $this->swtPassword));
-
+        $hashedData = strtoupper(hash('sha256', $requestId . $this->swtId . $this->userId . $timestamp . $this->swtPassword));
         $body['header'] = [
             'requestId' => $requestId,
             'swtId' => $this->swtId,
-            'userId' => $userId,
+            'userId' => $this->userId,
             'hashedData' => $hashedData,
-            'timestamp' => $timestamp
-        ];
+            'timestamp' => $timestamp,
 
+        ];
+        $body['additionalData'] = $this->helper->generateUUIDv4();
+        if(!empty($params)){
+            $body = array_merge($body, $params);
+        }
+        
         try {
             $response = $this->client->post($endpoint, [
-                'headers' => ['Content-Type' => 'application/json'],
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => $this->helper->generateBasicAuthHeader($this->serviceUser, $this->servicePass)
+                ],
                 'json' => $body
             ]);
 
             $data = json_decode($response->getBody(), true);
-
-            if (isset($data['errorCode'])) {
-                throw PaymentException::apiError($data['errorMessage']);
+            $data['payResult']['requestId'] =  $requestId;
+            $data['payResult']['returnCode']  =$data['header']['returnCode'] ;
+            $data['payResult']['reasonCode'] = $data['header']['reasonCode'] ;
+            $data['payResult']['message'] = $data['header']['message'] ;
+            $data['payResult']['additionalData']=$body['additionalData'] ;
+            unset($data['header']);
+            if (isset($data['errorMap']) && is_array($data['errorMap'])) {
+                throw PaymentException::fromErrorMap($data['errorMap']);
             }
+
 
             return $data;
         } catch (RequestException $e) {
