6f1304590b925903c8351f796301f800e6271b2f
peace1313 <122394469+peace1313@users.noreply.github.com>
2025-03-05 22:35:49 +0300

    Add files via upload

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -4,13 +4,15 @@ namespace Peace1313\GPaymentSdk\Config;
 
 class Config
 {
-    public static function getEnv($key, $default = null)
-    {
-        return getenv($key) ?: $default;
-    }
 
-    const API_URL = self::getEnv('API_URL', 'https://api.example.com');
-    const SWT_ID = self::getEnv('SWT_ID', 'CC82C381E078482AB328943FCCB7100C');
-    const SWT_PASSWORD = self::getEnv('SWT_PASSWORD', '123asdASD@');
-    const BANK_ID = self::getEnv('BANK_ID', '62');
+
+    const API_URL =  'https://gbtaksimtunel-integration.garanti.com.tr';
+    const SWT_ID =  'CC82C381E078482AB328943FCCB7100C';
+    const SWT_PASSWORD = '123asdASD@';
+    const USER_ID='damla123';
+    const REDIRECT_URL="https://eticaret.garanti.com.tr/destek/postback.aspx";
+    const BANK_ID =  62;
+    const SERVICE_USER = "srv_getCusTomNum";
+    const SERVICE_PASS = "Q88B75pZuG";
+
 }
