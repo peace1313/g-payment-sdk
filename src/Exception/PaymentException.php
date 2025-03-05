a92076535d9fd0f1230d8ddd4d753b7ce82edf30
peace1313 <122394469+peace1313@users.noreply.github.com>
2025-03-05 22:34:41 +0300

    Add files via upload

--- src/Exception/PaymentException.php
+++ src/Exception/PaymentException.php
@@ -6,6 +6,22 @@ use Exception;
 
 class PaymentException extends Exception
 {
+    private array $errorData;
+
+    public function __construct(string $message, int $code = 400)
+    {
+        parent::__construct($message, $code);
+        $this->errorData = [
+            "success" => false,
+            "message" => $message
+        ];
+    }
+
+    public function getErrorData(): array
+    {
+        return $this->errorData;
+    }
+
     public static function invalidData($message)
     {
         return new self("Geçersiz veri: " . $message);
@@ -14,5 +30,20 @@ class PaymentException extends Exception
     public static function apiError($message)
     {
         return new self("API Hatası: " . $message);
+    }
+
+    public static function invalidCardNumber()
+    {
+        return new self("Geçersiz kart numarası!!", 400);
+    }
+
+    public static function fromErrorMap(array $errorMap)
+    {
+        $errorMessages = [];
+        foreach ($errorMap as $field => $message) {
+            $errorMessages[] = "$field: $message";
+        }
+
+        return new self("API Hatası: " . implode(", ", $errorMessages), 400);
     }
 }
